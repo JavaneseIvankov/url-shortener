a31d94306e31821ca297caae93b190bf0cd7e758
arundaya <runsworkspace@gmail.com>
2025-05-28 13:41:42 +0700

    feat: added edit shortink method in repo

--- internal/repostiories/shortlink_repository.go
+++ internal/repostiories/shortlink_repository.go
@@ -2,8 +2,8 @@ package repostiories
 
 import (
 	"errors"
+	"fmt"
 	"time"
-
 )
 
 func uuid() uint {
@@ -21,6 +21,7 @@ type IShortlinkRepository interface {
 	CreateRedirectLink(shortName string, url string) (*ShortLink, error)
 	GetRedirectLink(shortName string) (*ShortLink, error)
 	DeleteRedirectLink(shortName string) error
+	EditShortLink(shortName string, newUrl string) (*ShortLink, error)
 }
 
 type ShortLinkImpl struct {
@@ -30,7 +31,7 @@ type ShortLinkImpl struct {
 func NewShortLinkRepository() IShortlinkRepository {
 	return &ShortLinkImpl{
 		store: make(map[string]string),
-	}
+}
 } 
 
 func (s *ShortLinkImpl) CreateRedirectLink(shortName string, url string) (*ShortLink, error) {
@@ -62,7 +63,7 @@ func (s *ShortLinkImpl) GetRedirectLink(shortName string) (*ShortLink, error) {
 		id: uuid(),
 		ShortName: shortName,
 		OriginalUrl:  url,
-		createdAt: time.Now().Format("YYYY-MM-DD"),
+		createdAt: time.Now().Format("2006-12-06"),
 	}
 
 	return res, nil
@@ -76,4 +77,22 @@ func (s *ShortLinkImpl) DeleteRedirectLink(shortName string) (error) {
 
 	delete(s.store, shortName)
 	return nil;
+}
+
+func (s *ShortLinkImpl) EditShortLink(shortName string, newUrl string) (*ShortLink, error) {
+	_, exists := s.store[shortName]
+	if !exists {
+		fmt.Println("LINK DOESN'T EXISTS")
+		return nil, errors.New("Link doesn't exist")
+	}
+
+	s.store[shortName] = newUrl
+	res := &ShortLink{
+		id: uuid(),
+		ShortName: shortName,
+		OriginalUrl: newUrl,
+		createdAt: time.Now().Format("2006-12-06"),
+	}
+
+	return res, nil
 }
